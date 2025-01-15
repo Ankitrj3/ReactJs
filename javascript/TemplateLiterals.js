4ea03c31a899fbc6099aa01c5b70d65e6482f452
ankitrj3 <ankit.12000777@lpu.in>
2025-01-15 17:09:33 +0530

    String Interpolation Day 2

---
+++ javascript/TemplateLiterals.js
@@ -0,0 +1,19 @@
+// String Interpolation
+
+// old way
+let name2 = "LPU";
+console.log("Welcome to " + name2);
+
+// String.concat()
+let str1 = "Hello";
+console.log(str1.concat(" ", "World"));
+
+// new es6 feature
+let name1 = "LPU";
+console.log(`Welcome to ${name1}`);
+
+let state = 'punjab';
+console.log(`I am from ${state}`);
+
+let friend = 'ujjwal';
+console.log(`My friend name is ${friend}`);
