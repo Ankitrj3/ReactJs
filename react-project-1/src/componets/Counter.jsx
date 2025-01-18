0ae5792d4c94c5b8b1d2c12b7dab023e6e0228da
ankitrj3 <ankit.12000777@lpu.in>
2025-01-18 11:14:31 +0530

    react-interface-intro

---
+++ react-project-1/src/componets/Counter.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const incrementCounter = () => setCount(count + 1);
+    const decrementCounter = () => setCount(count - 1);
+  return (
+    <div>
+      <button onClick={incrementCounter}>+</button>
+        <span>{count}</span>
+      <button onClick={decrementCounter}>-</button>
+    </div>
+  )
+}
+
+export default Counter
