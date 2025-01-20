0a5c920ee29fc24bc778e2e63742fddef90a0276
ankitrj3 <ankit.12000777@lpu.in>
2025-01-20 19:39:22 +0530

    Add Material-UI NavBar

--- SocialApp/src/App.jsx
+++ SocialApp/src/App.jsx
@@ -3,30 +3,44 @@ import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import "./App.scss";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
 import LeftBar from "./componets/leftBar/LeftBar";
 import NavBar from "./componets/navbar/NavBar";
 import RightBar from "./componets/rightBar/RightBar";
 
 function App() {
+
+  const currentUser = true;
+
   const Layout = () => {
     return (
       <div>
         <NavBar/>
         <div style={{display:"flex"}}>
           <LeftBar/>
-          <Outlet/>
+          <Outlet/>{/* Outlet is a placeholder for the child routes*/}
           <RightBar/>
         </div>
       </div>
     )
   }
+  const ProtectedRoute = ({children})=>{
+    if(!currentUser){
+      return <Navigate to="/login"/>
+    }
+    return children;
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
-      children: [
+      
+      element: (
+      <ProtectedRoute>
+         <Layout />
+      </ProtectedRoute>
+      ),
+      children:[
         {
         path: "/",
         element: <Home />,
@@ -45,14 +59,7 @@ function App() {
       path: "/register",
       element: <Register />,
     },
-    // {
-    //   path: "/profile",
-    //   element: <Profile />,
-    // },
-    // {
-    //   path: "/home",
-    //   element: <Home />,
-    // },
+  
   ]);
 
   return (
--- SocialApp/src/componets/navbar/NavBar.jsx
+++ SocialApp/src/componets/navbar/NavBar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import './navBar.scss'
 import RightBar from '../rightBar/RightBar'
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import GridViewIcon from '@mui/icons-material/GridView';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
+import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
-    <div>
-      <h1>navBar</h1>
+    <div className='navbar'>
+      <div className='left'>
+        <Link to='/' style={{textDecoration:'none'}}>
+        <span>SOCIALBits...</span>
+        </Link>
+        <HomeIcon/>
+        <BedtimeIcon/>
+        <GridViewIcon/>
+        <div className='search'>
+          <SearchIcon/>
+          <input type='text' placeholder='Search for friends, posts or videos'/>
+        </div>
+      </div>
+      
+      <div className='right'>
+        <PersonIcon/>
+        <MailIcon/>
+        <NotificationsIcon/>
+        <div className='user'>
+          <img src="https://cdn-icons-png.flaticon.com/512/219/219988.png" alt="" />
+          <span>Ankit Ranjan</span>
+        </div>
+      </div>
     </div>
   )
 }
